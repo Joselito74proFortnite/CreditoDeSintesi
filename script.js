61f0b3ce2ab19246bade85edc15c693ae85a69e0
Joselito74proFortnite <josejimenez@elpuig.xeill.net>
2024-05-24 13:26:06 +0200

    fuckfuck

---
+++ script.js
@@ -0,0 +1,20 @@
+
+$(function(){
+    $(window).scroll(function(){
+        var winTop = $(window).scrollTop();
+        if(winTop >= 30){
+            $("body").addClass("sticky-header");
+        }else{
+            $("body").removeClass("sticky-header");
+        }
+    })
+})
+
+
+fetch("./header.html")
+  .then(response => {
+    return response.text()
+  })
+  .then(data => {
+    document.querySelector("header").innerHTML = data;
+  });
